7b460b2f40472a714f4f1497d2c4122827131f39
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-07 14:06:14 +0200

    Create Home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../assets/css/styles.css'; // Import global styles
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to QuizzifyPro!</h2>
+      <p>
+        QuizzifyPro is a platform that allows you to create and share interactive quizzes and surveys. Get started by
+        navigating to the Quiz Creator or Survey Creator in the navigation menu.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
