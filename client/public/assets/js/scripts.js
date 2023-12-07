ca703994d24a490084a7e71b87432377058f8161
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-07 13:45:28 +0200

    Create scripts.js

---
+++ client/public/assets/js/scripts.js
@@ -0,0 +1,7 @@
+// Global JavaScript scripts
+document.addEventListener('DOMContentLoaded', function () {
+  // Your initialization code here
+
+  // Example: Change the background color of the body
+  document.body.style.backgroundColor = '#f0f0f0';
+});
